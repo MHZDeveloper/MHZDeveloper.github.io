737181c6ad8f34d015a1e74d1817824d79511d2e
MHZ <mohamed.hichem.zayani@gmail.com>
2024-04-24 18:18:23 +0100

    feat: init website

---
+++ assets/js/main.js
@@ -0,0 +1,125 @@
+"use strict";
+
+
+jQuery(document).ready(function ($) {
+
+
+    /*---------------------------------------------*
+     * Mobile menu
+     ---------------------------------------------*/
+    $(document).on('click', '.navbar-collapse.in', function (e) {
+        if ($(e.target).is('a')) {
+            $(this).collapse('hide');
+        }
+    });
+
+    /*---------------------------------------------*
+     * STICKY scroll
+     ---------------------------------------------*/
+
+    $.localScroll();
+
+
+
+    /*---------------------------------------------*
+     * WOW
+     ---------------------------------------------*/
+
+    var wow = new WOW({
+        mobile: false // trigger animations on mobile devices (default is true)
+    });
+    wow.init();
+
+
+    /*---------------------------------------------*
+     * Counter 
+     ---------------------------------------------*/
+
+    $('.statistic-counter').counterUp({
+        delay: 10,
+        time: 2000
+    });
+
+
+
+
+
+
+    /* ---------------------------------------------------------------------
+     Carousel
+     ---------------------------------------------------------------------= */
+
+    $('.main_home_slider').owlCarousel({
+        responsiveClass: true,
+        autoplay: true,
+        autoplaySpeed: 1000,
+        autoplayTimeout: 3000,
+        items: 1,
+        loop: true,
+        dots: true,
+        nav: false,
+        navText: [
+            "<i class='lnr lnr-chevron-left'></i>",
+            "<i class='lnr lnr-chevron-right'></i>"
+        ],
+        autoplayHoverPause: false
+
+    });
+
+
+
+
+// main-menu-scroll
+
+    jQuery(window).scroll(function () {
+        var top = jQuery(document).scrollTop();
+        var height = 300;
+        //alert(batas);
+
+        if (top > height) {
+            jQuery('.navbar-fixed-top').addClass('menu-scroll');
+        } else {
+            jQuery('.navbar-fixed-top').removeClass('menu-scroll');
+        }
+    });
+
+// scroll Up
+
+    $(window).scroll(function () {
+        let scrollTop = $(this).scrollTop();
+        if (scrollTop > 600) {
+            $('.scrollup').fadeIn('slow');
+        } else {
+            $('.scrollup').fadeOut('slow');
+        }
+
+    });
+
+    $('.scrollup').click(function () {
+        $("html, body").animate({scrollTop: 0}, 1000);
+        return false;
+    });
+
+
+
+//    $('#menu').slicknav();
+
+    $('#mixcontent').mixItUp({
+        animation: {
+            animateResizeContainer: false,
+            effects: 'fade rotateX(-45deg) translateY(-10%)'
+        }
+    });
+
+
+
+
+    //End
+});
+
+
+
+
+
+
+
--- js/script.js
+++
@@ -1,9 +0,0 @@
-$(window).scroll(function() {
-    var scroll = $(window).scrollTop();
-
-    //>=, not <=
-    if (scroll >= 500) {
-        //clearHeader, not clearheader - caps H
-        $('.scroll-animations').addClass('animated fadeInUp');
-    }
-});
